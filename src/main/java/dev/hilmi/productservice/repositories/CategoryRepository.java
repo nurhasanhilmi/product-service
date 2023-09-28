8f3ab29ff4d50353679950bd335a9b0dbf615952
Ahmad Nurhasan Hilmi <nurhasanhilmi@gmail.com>
2023-09-28 13:20:31 +0700

    Add Category and Product repositories

---
+++ src/main/java/dev/hilmi/productservice/repositories/CategoryRepository.java
@@ -0,0 +1,10 @@
+package dev.hilmi.productservice.repositories;
+
+import dev.hilmi.productservice.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Optional<Category> findByName(String name);
+}
---
+++ src/main/java/dev/hilmi/productservice/repositories/ProductRepository.java
@@ -0,0 +1,11 @@
+package dev.hilmi.productservice.repositories;
+
+import dev.hilmi.productservice.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    Optional<Product> findByCode(String code);
+}
