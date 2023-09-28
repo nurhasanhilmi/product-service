0279cf47710cbbad53dec14cbafeb7d6f41cd1ad
Ahmad Nurhasan Hilmi <nurhasanhilmi@gmail.com>
2023-09-28 13:23:00 +0700

    Add OpenApi definition

--- src/main/java/dev/hilmi/productservice/ProductServiceApplication.java
+++ src/main/java/dev/hilmi/productservice/ProductServiceApplication.java
@@ -1,9 +1,14 @@
 package dev.hilmi.productservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(title = "Product Service", version = "0.0.1-SNAPSHOT")
+)
 public class ProductServiceApplication {
 
     public static void main(String[] args) {
